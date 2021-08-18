65e9f9b9fe50c319c7b82e25220f91d1b2e6a911
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-18 23:50:38 +0900

    テーブルのカラムに制約を入れ、マイグレーションを実行

--- db/migrate/20210818144839_create_messages.rb
+++ db/migrate/20210818144839_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.text :content
+      t.string :title, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
