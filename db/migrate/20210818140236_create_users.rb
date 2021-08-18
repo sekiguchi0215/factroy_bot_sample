4ef4bccf98ad0d1908f00740b1b05f89b59a56a9
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-18 23:06:47 +0900

    テーブルのカラムに制約を入れ、マイグレーションを実行

--- db/migrate/20210818140236_create_users.rb
+++ db/migrate/20210818140236_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.intger :age
-      t.string :email
+      t.string :name, null: false
+      t.integer :age, null: false
+      t.string :email, null: false
 
       t.timestamps
     end
